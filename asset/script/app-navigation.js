beb36ded361917949f5e363bbb0bc94ba64d1a7c
Adarsh Kumar <adarashKumar9002@gmail.com>
2024-08-06 16:45:03 +0530

    Improve Book navigation menu accessibility and add functionality to toggle book list
    - index.html
      - Added aria-expanded and aria-controls to Book button in header-nav for better accessibility
      - Added id="book-list-btn" to Book button
      - Added id="book-list" to book-list nav tag
      - Added data-book-list-visible attribute to book-list nav tag
    
    - app-navigation.js
      - Created logic for toggling book list, and updating accessibility attributes

--- asset/script/app-navigation.js
+++ asset/script/app-navigation.js
@@ -1,7 +1,9 @@
 const MENU_BUTTON_ELEMENT = document.getElementById('menu-btn');
 const HEADER_NAVIGATION_MENU_ELEMENT = document.getElementById('header-nav');
-
-
+const BOOK_NAV_BUTTON_ELEMENT =
+  HEADER_NAVIGATION_MENU_ELEMENT.querySelector('#book-list-btn');
+const BOOK_NAV_LIST_ELEMENT =
+  HEADER_NAVIGATION_MENU_ELEMENT.querySelector('#book-list');
 
 /* ========== Header Navigation ========== */
 
@@ -10,27 +12,69 @@ const updateMenuButtonIcon = (removeElementClassName, addElementClass) => {
   MENU_BUTTON_ELEMENT.classList.add(addElementClass);
 };
 
-const updateAccessibilityAttributes = (attributesValue) => {
-  MENU_BUTTON_ELEMENT.setAttribute('aria-expanded', attributesValue);
-  HEADER_NAVIGATION_MENU_ELEMENT.setAttribute('data-visible', attributesValue);
+const updateAccessibilityAttributes = (
+  firstElement,
+  secondElement,
+  attributesValue,
+) => {
+  firstElement.setAttribute('aria-expanded', attributesValue);
+  secondElement.setAttribute('data-visible', attributesValue);
 };
 
 const toggleNav = () => {
-  const isNavVisible = HEADER_NAVIGATION_MENU_ELEMENT.getAttribute('data-visible');
+  const isNavVisible =
+    HEADER_NAVIGATION_MENU_ELEMENT.getAttribute('data-visible');
 
   if (isNavVisible === 'false') {
     updateMenuButtonIcon('menu-icon-open', 'menu-icon-close');
 
     HEADER_NAVIGATION_MENU_ELEMENT.classList.add('visible');
 
-    updateAccessibilityAttributes('true');
+    updateAccessibilityAttributes(
+      MENU_BUTTON_ELEMENT,
+      HEADER_NAVIGATION_MENU_ELEMENT,
+      'true',
+    );
   } else {
     updateMenuButtonIcon('menu-icon-close', 'menu-icon-open');
 
     HEADER_NAVIGATION_MENU_ELEMENT.classList.remove('visible');
 
-    updateAccessibilityAttributes('false');
+    updateAccessibilityAttributes(
+      MENU_BUTTON_ELEMENT,
+      HEADER_NAVIGATION_MENU_ELEMENT,
+      'false',
+    );
   }
 };
 
-export { MENU_BUTTON_ELEMENT, toggleNav };
+/* ========== Book list ========== */
+
+const toggleBookList = () => {
+  const isBookListVisible = BOOK_NAV_LIST_ELEMENT.getAttribute('data-visible');
+
+  if (isBookListVisible === 'false') {
+    BOOK_NAV_LIST_ELEMENT.classList.add('visible');
+
+    updateAccessibilityAttributes(
+      BOOK_NAV_BUTTON_ELEMENT,
+      BOOK_NAV_LIST_ELEMENT,
+      'true',
+    );
+  } else {
+    BOOK_NAV_LIST_ELEMENT.classList.remove('visible');
+
+    updateAccessibilityAttributes(
+      BOOK_NAV_BUTTON_ELEMENT,
+      BOOK_NAV_LIST_ELEMENT,
+      'false',
+    );
+  }
+};
+
+export {
+  MENU_BUTTON_ELEMENT,
+  BOOK_NAV_BUTTON_ELEMENT,
+  toggleNav,
+  toggleBookList,
+};
--- asset/script/app.js
+++ asset/script/app.js
@@ -1,3 +1,9 @@
-import { MENU_BUTTON_ELEMENT, toggleNav } from "./app-navigation.js";
+import {
+  MENU_BUTTON_ELEMENT,
+  BOOK_NAV_BUTTON_ELEMENT,
+  toggleNav,
+  toggleBookList,
+} from './app-navigation.js';
 
 MENU_BUTTON_ELEMENT.addEventListener('click', toggleNav);
+BOOK_NAV_BUTTON_ELEMENT.addEventListener('click', toggleBookList);
